675ae30efb1fa6d7154ebd54ed9e36e9417e1f30
ricardosantanadev <ricardosantanadev@gmail.com>
2023-02-07 15:08:51 -0300

    update

--- src/app/tickets/tickets/tickets.component.ts
+++ src/app/tickets/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Tickets } from '../model/tickets';
+import { TicketService } from '../services/ticket.service';
 
 @Component({
   selector: 'app-tickets',
@@ -7,6 +8,11 @@ import { Tickets } from '../model/tickets';
   styleUrls: ['./tickets.component.scss']
 })
 export class TicketsComponent {
-  dataSource: Tickets[] = [{ status: '', id: '', assunto: '', categoria: '', tecnico: '', nivel: '', solicitante: '', criacao: '', urgencia: '', vencimento: '' }];
+  // dataSource: Tickets[] = [{ status: 'Aberto', id: '1', assunto: 'Softphone - Instalar,Configuração', categoria: 'Dispositivo', tecnico: 'Suporte Técnico - 01', nivel: 'Nível 1', solicitante: 'helpdesk@rsds.com.br', criacao: '07/02/2023', urgencia: 'Baixa', vencimento: '10/02/2023 10:20' }];
+  dataSource: Tickets[] = [];
   displayedColumns = ['status', 'id', 'assunto', 'categoria', 'tecnico', 'nivel', 'solicitante', 'criacao', 'urgencia', 'vencimento']
+  
+  constructor(private ticketService: TicketService) {
+    this.dataSource = ticketService.getTickets();
+  }
 }
