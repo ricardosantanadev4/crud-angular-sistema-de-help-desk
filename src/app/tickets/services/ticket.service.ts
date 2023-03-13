babe958b36e3eb461c5a1f2f029f3a515321888c
ricardosantanadev <ricardosantanadev@gmail.com>
2023-03-13 15:55:16 -0300

    update

--- src/app/tickets/services/ticket.service.ts
+++ src/app/tickets/services/ticket.service.ts
@@ -8,7 +8,9 @@ import { Tickets } from '../model/tickets';
 })
 export class TicketService {
   // dataSource: Tickets[] = [{ status: 'Aberto', id: '1', assunto: 'Softphone - Instalar,Configuração', categoria: 'Dispositivo', tecnico: 'Suporte Técnico - 01', nivel: 'Nível 1', solicitante: 'helpdesk@rsds.com.br', criacao: '07/02/2023', urgencia: 'Baixa', vencimento: '10/02/2023 10:20' }];
-  private readonly API = 'http://localhost:3000/values';
+  // private readonly API = 'http://localhost:3000/values';
+  private readonly API = '/api/Tickets-list';
+
   constructor(private httpClient: HttpClient) { }
 
   getTickets() {
