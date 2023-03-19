5c65aad21d5b0b20e496a664db736b8ff9cc60b4
ricardosantanadev <ricardosantanadev@gmail.com>
2023-03-19 14:23:00 -0300

    update

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -6,6 +6,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   // declarations: [],
@@ -18,7 +21,10 @@ import { MatIconModule } from '@angular/material/icon';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AppMaterialModule { }
--- src/app/tickets/tickets-form/tickets-form.component.ts
+++ src/app/tickets/tickets-form/tickets-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tickets-form',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./tickets-form.component.scss']
 })
 export class TicketsFormComponent {
+  form: FormGroup;
 
+  constructor(private formBuild: FormBuilder) {
+    this.form = formBuild.group({
+      status: ['ABERTO'],
+      // id: string;
+      assunto: [null],
+      categoria: [null],
+      tecnico: [null],
+      nivel: [null],
+      solicitante: [null],
+      criacao: [null],
+      urgencia: [null],
+      vencimento: [null],
+    });
+    
+  }
 }
